fff78a9c083ddd4e18843c6e45491d8ef9c8a14d
Roberto Clapis <empijei@users.noreply.github.com>
2021-02-03 01:02:27 +0100

    Add host-based state persistence.

--- background.js
+++ background.js
@@ -1,11 +1,27 @@
-chrome.browserAction.onClicked.addListener(function (tab) {
-    chrome.tabs.executeScript(tab.id,
-        {
-            code: `
-        if (document.body.classList.contains('invert')) {
-            document.body.classList.remove('invert');
-        } else {
-            document.body.classList.add('invert');
+chrome.storage.sync.get('state', (state) => {
+    if (!(state instanceof Map)) state = new Map();
+
+    chrome.browserAction.onClicked.addListener(function (tab) {
+        const domain = (new URL(tab.url)).hostname;
+        const active = !state.get(domain);
+        state.set(domain, active);
+        chrome.storage.sync.set({ 'state': state });
+        chrome.tabs.executeScript(tab.id,
+            {
+                code: `document.body.classList.${active ? "add" : "remove"}('invert')`
+            }
+        );
+    });
+
+    chrome.tabs.onUpdated.addListener(function (tabId, changeInfo, tab) {
+        if (changeInfo.status != 'complete') {
+            return;
         }
-    `});
+        const domain = (new URL(tab.url)).hostname;
+        const active = state.get(domain);
+        if (!active) {
+            return;
+        }
+        chrome.tabs.executeScript(tab.id, { code: `document.body.classList.add('invert')` });
+    })
 });
